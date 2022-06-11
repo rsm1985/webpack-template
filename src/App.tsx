363f2db0eb20dfe70efa9aa28d7c0e40ae645466
Sergey Rudenko <rudenko-serg@ya.ru>
2022-06-11 07:48:40 +0300

    Add hot module replacement

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import React, {memo, FC} from 'react';
 import img from "./images/bg.png"
 import check from "./images/checked.svg"
+import {Counter} from "./Counter";
 export const App = ({}) => {
     console.log("process.env", process.env)
     return (
         <>
             <h1>
-                Hello
+                Hello!
 
             </h1>
-            <img src={img} alt=""/>
-            <img src={check} alt=""/>
+            {/*<img src={img} alt=""/>*/}
+            {/*<img src={check} alt=""/>*/}
+            <Counter />
         </>
 
     );
--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,11 +1,18 @@
 const webpack = require("webpack")
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+
 require('dotenv').config({ path: './.env' });
 module.exports = {
     mode: "development",
     devtool: "cheap-module-source-map",
+    devServer: {
+        hot: true,
+        open: true
+    },
     plugins: [
         new webpack.DefinePlugin({
             'process.env': JSON.stringify(process.env),
         }),
+        new ReactRefreshWebpackPlugin()
     ]
 }
