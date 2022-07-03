e3f2ffc3d235c5148edcbb15f7543e6d68327da7
Sergey Rudenko <rudenko-serg@ya.ru>
2022-07-03 16:01:51 +0300

    Setup webpack config

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,14 +1,5 @@
-const webpack = require("webpack")
-require('dotenv').config({ path: './.env.prod' });
-
-
 module.exports = {
-    mode: "production",
-    devtool: "source-map",
-    plugins: [
-        new webpack.DefinePlugin({
-            'process.env': JSON.stringify(process.env),
-        }),
-
-    ],
+  mode: 'production',
+  devtool: 'source-map',
+  plugins: [],
 }
