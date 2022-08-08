4458732a6adaddf01e8e17f5c75c8965148621f6
Sergey Rudenko <sergey.rudenko@websailors.pro>
2022-08-08 10:56:11 +0300

    Add tsconfig exclude

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Index } from '@/components/Counter'
 import { StyledWrapper } from '@/components/Counter/styles'
-// import Image from '@/assets/images/bg'
+// import { Image } from '@/assets/images'
 export const App = () => {
   return (
     <StyledWrapper>
-      <h1>Hello!!!!!!!!</h1>
+      <h1>Hello!!!!!</h1>
       {/* <img src={Image} alt='' /> */}
       <div>1234</div>
       <Index />
